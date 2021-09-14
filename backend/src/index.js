e9cbc266d055aca40d6bc89548f38e1467212cac
Alejandro Palacios <Alejandro.Palacios@fundacion-jala.org>
2021-09-13 21:47:00 -0500

    Added base for swagger support

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const { Pool, Client } = require('pg');
 const cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsDoc = require("swagger-jsdoc");
 const usuarioRoutes = require('./routes/usuarios');
 const obraRoutes = require('./routes/obras');
 const clienteRoutes = require('./routes/clientes');
@@ -14,6 +16,19 @@ const connectionString = 'postgressql://registro_aca:lina159@localhost:5432/sist
 const client = new Client({
     connectionString: connectionString
 })
+
+const swaggerOptions = {
+    swaggerDefinition: {
+      info: {
+        version: "1.0.0",
+        title: "Sistema Gestion de Obras",
+        description: "SGO API Information",
+        servers: ["http://localhost:3000"]
+      }
+    },
+    // ['.routes/*.js']
+    apis: ["app.js"]
+  };
 
 const app = express();
 
@@ -29,6 +44,8 @@ app.set('port', process.env.PORT || 3000);
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(cors());
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 //Routes
 app.use('/usuarios', usuarioRoutes);
